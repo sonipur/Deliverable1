09351086be5bb47adf8e0d96d17b0220a807d3bf
rajae <barnesra@sheridancollege.ca>
2023-08-14 20:06:42 -0400

    Updated WarGame

--- src/deliverable3/WarGame.java
+++ src/deliverable3/WarGame.java
@@ -37,45 +37,86 @@ public class WarGame {
 
  
 
-    private String determineRoundWinner(List<Card> player1Round, List<Card> player2Round) {
-        int comparison = cardComparator.compare(player1Round.get(3), player2Round.get(3));
-        if (comparison > 0) {
-            player1.addCards(player1Round);
-            player1.addCards(player2Round);
-            return player1.getName();
-        } else if (comparison < 0) {
-            player2.addCards(player1Round);
-            player2.addCards(player2Round);
-            return player2.getName();
-        } else {
-            return "War!";
-        }
+
+
+
+
+
+
+private String determineRoundWinner(List<Card> player1Round, List<Card> player2Round) {
+    if (player1Round.size() < 4 || player2Round.size() < 4) {
+        return "Not enough cards for a round!";
     }
 
+    int comparison = cardComparator.compare(player1Round.get(3), player2Round.get(3));
+    if (comparison > 0) {
+        return player1.getName();
+    } else if (comparison < 0) {
+        return player2.getName();
+    } else {
+        return "War!";
+    }
+}
 
 
-    public void play() {
-        int round = 1;
-        while (player1.getNumCards() >= 4 && player2.getNumCards() >= 4 && round <= 52) {
-            List<Card> player1Round = player1.drawCards(4);
-            List<Card> player2Round = player2.drawCards(4);
+public void play() {
+    int round = 1;
+    while (player1.getNumCards() >= 4 && player2.getNumCards() >= 4 && round <= 52) {
+        List<Card> player1Round = player1.drawCards(4);
+        List<Card> player2Round = player2.drawCards(4);
 
-            String roundWinner = determineRoundWinner(player1Round, player2Round);
-            System.out.println("Round " + round + " winner: " + roundWinner);
+        String roundWinner = determineRoundWinner(player1Round, player2Round);
+        System.out.println("Round " + round + " winner: " + roundWinner);
 
-            if (roundWinner.equals("War!")) {
-                player1Round.addAll(player1.drawCards(2));
-                player2Round.addAll(player2.drawCards(2));
+        if (roundWinner.equals("War!")) {
+            if (player1.getNumCards() < 10 || player2.getNumCards() < 10) {
+                // Handle game over or tie when not enough cards for war
+                // ...
             }
 
-            round++;
-            System.out.println(player1.getName() + " cards: " + player1.getNumCards() + " | " +
-                               player2.getName() + " cards: " + player2.getNumCards());
-            System.out.println();
+            List<Card> player1WarCards = player1.drawCards(10);
+            List<Card> player2WarCards = player2.drawCards(10);
+
+            String warWinner = determineRoundWinner(player1WarCards, player2WarCards);
+
+            if (warWinner.equals(player1.getName())) {
+                player1.addCards(player1Round);
+                player1.addCards(player2Round);
+                player1.addCards(player1WarCards);
+                player1.addCards(player2WarCards);
+            } else if (warWinner.equals(player2.getName())) {
+                player2.addCards(player1Round);
+                player2.addCards(player2Round);
+                player2.addCards(player1WarCards);
+                player2.addCards(player2WarCards);
+            } else {
+                // Handle tie case for war
+                // ...
+            }
+        } else {
+            if (roundWinner.equals(player1.getName())) {
+                player1.addCards(player1Round);
+                player1.addCards(player2Round);
+            } else {
+                player2.addCards(player1Round);
+                player2.addCards(player2Round);
+            }
         }
 
-        handleGameOver();
+        round++;
+        System.out.println(player1.getName() + " cards: " + player1.getNumCards() + " | " +
+                           player2.getName() + " cards: " + player2.getNumCards());
+        System.out.println();
     }
+
+    handleGameOver();
+}
+
+
+
+
+
+
 
     private void handleGameOver() {
         String winner = WinnerDeterminer.determineWinner(player1.getNumCards(), player2.getNumCards());
